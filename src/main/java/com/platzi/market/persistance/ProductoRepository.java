686b1f4b090a5c38fadd0ae2bba58909e0e0455b
marcelovelar <fovelar@pol.una.py>
2023-10-06 16:36:57 -0300

    Añadimos Map struct a la aplicación

--- src/main/java/com/platzi/market/persistance/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistance/ProductoRepository.java
@@ -2,16 +2,44 @@ package com.platzi.market.persistance;
 
 import com.platzi.market.persistance.crud.ProductoCrudRepository;
 import com.platzi.market.persistance.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+//@Repository : Con esta anotación le indicamos que esta clase se encarga de interactuar con la base de datos
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
-    public List<Producto>getAll(){ return (List<Producto>) productoCrudRepository.findAll();}
+
+    public List<Producto>getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();}
 
     public List<Producto> getByCategoria (int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+    public Optional<List<Producto>> getFalses(boolean b){
+        return productoCrudRepository.findByEstado(false);
+    }
+    public Optional<List<Producto>> getCheaperThan(int precio,int categoria){
+        return productoCrudRepository.findByPrecioVentaLessThanAndByIdCategoria(precio,categoria);
+    }
+
+    public Optional<Producto> getProducto(int id){
+        return productoCrudRepository.findById(id);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 
 }
--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -1,12 +1,21 @@
 package com.platzi.market.persistance.crud;
 
 import com.platzi.market.persistance.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+    //Esta forma sería con el query nativo
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
 
+    //De esta otra es con los Query Methods, más conveniente
     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
 
-}
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+    Optional<List<Producto>> findByEstado(boolean b);
+    Optional<List<Producto>> findByPrecioVentaLessThanAndByIdCategoria(int precioVenta,int idCategoria);
+
+ }
